e6f0f170dbf7818c5d1c72c5809cf2a76b4c7810
Senyo Simpson <31822321+senyosimpson@users.noreply.github.com>
2025-08-03 23:57:11 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export default {
 	async fetch(request): Promise<Response> {
-		const destinationURL = "https://example.com";
+		const destinationURL = "https://senyosimpson.github.io/letter";
 		const statusCode = 301;
 		return Response.redirect(destinationURL, statusCode);
 	},
